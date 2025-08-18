a3f92978060967bae782989979cb65221604a3fa
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-08-19 07:16:30 +0900

    feat: 회원가입 로그인 메뉴바

--- moem-app/src/App.jsx
+++ moem-app/src/App.jsx
@@ -1,16 +1,20 @@
-import './App.css'
 import logo from './assets/logo.png'
+import HomeButton from './components/HomeButton'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='header-container'>
         <img src={logo} className="logo-img"/>
+        <div >
+        <HomeButton/>
+        </div>
       </div>
-     
+      
     </>
-  )
-}
+  );
+};
 
 export default App
---
+++ moem-app/src/components/HomeButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomeButton = () => {
+  return (
+    <div class="btn">
+        <a>로그인</a>
+        <button><a>회원가입</a></button>
+    </div>
+  )
+}
+
+export default HomeButton
