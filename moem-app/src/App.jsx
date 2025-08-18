263f67b774237aac972372396a0581e2c52509bb
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-08-18 16:08:20 +0900

    feat: 홈 화면에 로고 사진 넣기

--- moem-app/src/App.jsx
+++ moem-app/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import logo from './assets/logo.png'
 
 function App() {
 
   return (
     <>
-      
+      <div>
+        <img src={logo} className="logo-img"/>
+      </div>
+     
     </>
   )
 }
