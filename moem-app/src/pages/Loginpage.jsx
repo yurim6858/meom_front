7a9fe2c7dd997e5a62053ec8f320e257bf0f4f29
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-08-19 14:39:43 +0900

    style:로그인 로고 css 변경

--- moem-app/src/pages/Loginpage.jsx
+++ moem-app/src/pages/Loginpage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 import { Logincomponent } from '../components/Logincomponent'
 
 
 const Loginpage = () => {
   return (
     <div>
+        <img src={logo} className="login-logo"/>
         <Logincomponent/>
     </div>
   )
