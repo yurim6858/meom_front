d2016705d66f96671f138bb56f08dc569cda15a0
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-08-20 07:51:11 +0900

    feat:회원가입 페이지

--- moem-app/src/pages/Signuppage.jsx
+++ moem-app/src/pages/Signuppage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import Signupcomponent from '../components/Signupcomponent'
 
 export const Signuppage = () => {
   return (
     <div>
-        <h1>하이하이</h1>
+        <img src={logo} className="login-logo"/>
+        <Signupcomponent/>
     </div>
   )
 }
