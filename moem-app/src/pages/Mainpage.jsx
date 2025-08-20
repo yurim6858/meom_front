d27f7f6945ac8a4ee129a1db18a18133e01b7d15
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-08-20 10:31:57 +0900

    rename:Mainpage로 파일명 변경

---
+++ moem-app/src/pages/Mainpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+export const Mainpage = () => {
+  return (
+    <div>
+        <img src={logo} className="logo-img"/>
+    </div>
+  )
+}
