a4141186732be532a3ca6a0d8004a57e9f6cc1fc
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-08-20 10:19:32 +0900

    docs: 홈 기능 변경

--- moem-app/src/App.jsx
+++ moem-app/src/App.jsx
@@ -4,27 +4,20 @@ import LoginPage from "./pages/Loginpage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import { Signuppage } from './pages/Signuppage';
+import Home from "./pages/Home";
 
 function App() {
 
   return (
     <>
+   
     <Router>
-      <div className='header-container'>
-        <img src={logo} className="logo-img"/>
-        <div >
-        <HomeButton/>
-        </div>
-      </div>
-      
-      
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path='/signup' element={<Signuppage/>}/>
       </Routes>
-    </Router>
-        
-      
+    </Router>  
     </>
   );
 };
--- moem-app/src/pages/Home.jsx
+++ moem-app/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import HomeButton from '../components/HomeButton'
 
 const Home = () => {
   return (
-    <div className="Home">
-        
-    </div>
+    <div className='header-container'>
+        <img src={logo} className="logo-img"/>
+        <div >
+        <HomeButton/>
+        </div>
+      </div>
   )
 }
 
