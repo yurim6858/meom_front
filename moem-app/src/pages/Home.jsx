fd09a8456c506350dfb3ca101f1e79b2cd86d65d
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-08-20 10:56:19 +0900

    feat:홈 포스터 사진 추가

--- moem-app/src/pages/Home.jsx
+++ moem-app/src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import poster from '../assets/poster.png'
 import HomeButton from '../components/HomeButton'
 
 const Home = () => {
   return (
+    <div>
     <div className='header-container'>
         <img src={logo} className="logo-img"/>
         <div >
         <HomeButton/>
-        </div>
+        </div> 
       </div>
+      <img src={poster} className="poster-img"/>
+       </div>
   )
 }
 
